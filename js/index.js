fb00efda786b3e9a09ddaadababf28da28183dda
jain0054 <122372524+jain0054@users.noreply.github.com>
2023-01-17 09:53:42 -0500

    alert completed

--- js/index.js
+++ js/index.js
@@ -8,4 +8,21 @@ output += 'let b = 5;\n';
 output += 'let c;\n';
 output += '--------\n';
 
+output += 'a+b = ' + (a+b)+"\n";
+output += 'a-b = ' + (a-b)+"\n";
+output += 'a*b = ' + (a*b)+"\n";
+output += 'a/b = ' + (a/b)+"\n";
+output += 'a%b = ' + (a%b)+"\n";
+output += '(a+=b) = ' + (a+=b)+"\n";
+output += '(a-=b) = ' + (a-=b)+"\n";
+output += '(a*=b) = ' + (a*=b)+"\n";
+output += '(a/=b) = ' + (a/=b)+"\n";
+output += '(a%=b) = ' + (a%=b)+"\n";
+output += '(a==b) = ' + (a==b)+"\n";
+output += '(a!=b) = ' + (a!=b)+"\n";
+output += '(a>b) = ' + (a>b)+"\n";
+output += '(a<b) = ' + (a<b)+"\n";
+output += '(!a&&!c) = ' + (!a&&!c)+"\n";
+output += '(!a||!c) = ' + (!a||!c)+"\n";
+
 alert (output);
