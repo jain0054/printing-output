e510c1b0d01c420275775111056aa54466e5cb63
jain0054 <122372524+jain0054@users.noreply.github.com>
2023-01-20 18:37:44 -0500

    concatenation added

--- js/index.js
+++ js/index.js
@@ -26,3 +26,13 @@ output += '(!a&&!c) = ' + (!a&&!c)+"\n";
 output += '(!a||!c) = ' + (!a||!c)+"\n";
 
 alert (output);
+
+
+// In-line concatenation
+let first_name = "Mayank";
+let last_name = "Jain";
+let email = "jain0054@algonquinlive.com";
+
+let sentence = "My name is" + ' ' + first_name + ' ' + last_name + ' . ' + ' ' + "You can contact me at" + ' ' + email;
+
+console.log(sentence);
